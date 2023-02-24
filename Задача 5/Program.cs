34fde88005c33bf1bdb983078e6defc286df648e
Maxim <mkorotkov77@gmail.com>
2023-02-24 19:50:19 +0300

    HW_61

---
+++ Задача 5/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 5. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+using System;
+using static System.Console;
+
+Clear();
+
+int [,] GetArray()
+{
+    int[,] result = new int[4, 4];
+    int el= 0;
+    
+    for (int i = 0; i < 1; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+    break;
+    }
+    
+        for (int j = 3; j < 4; j++)
+    {
+        for (int i = 1; i < 4; i++)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+         break;
+    }
+
+    for (int i = 3; i < 4; i++)
+    {
+        for (int j = 2; j > -1; j--)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+         break;
+    }
+
+ for (int j = 0; j <1; j++)
+    {
+        for (int i = 2; i > 0; i--)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+    }
+
+for (int i = 1; i < 2; i++)
+    {
+        for (int j = 1; j < 3; j++)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+    break;
+    }
+
+for (int i = 2; i < 3; i++)
+    {
+        for (int j = 2; j > 0; j--)
+        {   el=el+1;
+            result[i, j] = el;
+        }
+    break;
+    }
+
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+PrintArray (GetArray());
---
+++ доп_плитка/Program.cs
@@ -0,0 +1,81 @@
+﻿// *Доп1
+// На днях Иван у себя в прихожей выложил кафель,
+// состоящий из квадратных черных и белых плиток.
+// Прихожая Ивана имеет квадратную форму 4х4, вмещающую 16 плиток.
+// Теперь Иван переживает, что узор из плиток, который у него получился,
+// может быть не симпатичным. С точки зрения дизайна симпатичным узором считается тот,
+// который не содержит в себе квадрата 2х2, состоящего из плиток одного цвета.
+// Входные данные:
+// BWBW
+// BBWB
+// WWBB
+// BWWW
+// Ответ:
+// Yes
+
+// Входные данные:
+// BBWB
+// BBWB
+// WWBW
+// BBWB
+// Ответ:
+// No
+
+
+Console.Clear();
+
+Console.WriteLine("Введите размер массива");
+int m = int.Parse(Console.ReadLine());
+
+
+char[,] TypeArray(int m)
+{
+    Console.WriteLine("Введите эл-ты массива");
+    char [,] newArray = new char [m,m];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+        newArray [i,j] = char.Parse(Console.ReadLine());
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(char [,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+        Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+bool DesignArray( char [,] A)
+{
+for (int i=0; i<A.GetLength(0)-1;i++)
+{
+    for (int j=0; j<A.GetLength(0)-1;j++)
+    {
+             if(A[i,j] == A[i+1,j] && A[i,j] == A[i,j+1] && A[i,j] == A[i+1,j+1])
+             {
+             return true;
+             break;
+             }
+    }
+}
+return false;
+}
+
+char [,] array = TypeArray(m);
+PrintArray(array);
+Console.WriteLine();
+
+if (DesignArray(array))
+{Console.WriteLine("ответ: yes");}
+else
+{Console.WriteLine("ответ: no");}
